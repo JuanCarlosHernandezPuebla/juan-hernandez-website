38eedbde2491457e199757bf190a01ccaec91ec6
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2025-01-14 03:21:36 -0600

    Add Navbar and Footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const year = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; {year} Juan Carlos Hernandez Puebla</p>
+    </footer>
+  );
+};
+
+export default Footer;
