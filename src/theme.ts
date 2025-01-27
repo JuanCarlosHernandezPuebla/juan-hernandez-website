eedd66123221de26d5216a077c241ef63333c58c
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2025-01-27 00:50:08 -0600

    Configure MUI theme for dark mode class selector

--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,30 @@
-"use client";
+'use client'
 
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
-  cssVariables: true,
-  typography: {
-    fontFamily: "var(--font-roboto)",
-  },
-});
+    typography: {
+        fontFamily: 'var(--font-roboto)',
+    },
+    colorSchemes: {
+        light: true,
+        dark: true,
+    },
+    cssVariables: {
+        colorSchemeSelector: 'class',
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                '*,::before,::after': {
+                    boxSizing: 'border-box',
+                    borderWidth: 0,
+                    borderStyle: 'solid',
+                    borderColor: "theme('borderColor.DEFAULT', currentColor)",
+                },
+            },
+        },
+    },
+})
 
-export default theme;
+export default theme
