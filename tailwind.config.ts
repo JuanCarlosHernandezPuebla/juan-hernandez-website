f38c41c603453cb81c5e8aac854eb081973a9c77
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2025-01-27 00:47:57 -0600

    Update tailwindcss config for manually toggling dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  corePlugins: {
-    preflight: false,
-  },
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  important: "#__next",
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    darkMode: 'selector',
+    corePlugins: {
+        preflight: false,
     },
-  },
-  plugins: [],
-} satisfies Config;
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    important: '#__next',
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config
